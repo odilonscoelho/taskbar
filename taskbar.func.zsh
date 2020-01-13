6af612e0fdf28f2ec260c7d227b99385b9698ef5
odilonscoelho <odilonscoelho@hotmail.com>
2020-01-13 16:36:11 -0400

    Atualização em program e func

--- taskbar.func.zsh
+++ taskbar.func.zsh
@@ -1,12 +1,15 @@
 #!/bin/zsh
-foco () {wmctrl -i -a "$(sed -n $1'p' $bd |awk {'print $2'})"}
-close () {wmctrl -i -c "$(sed -n $1'p' $bd |awk {'print $2'})"}
+foco.bkp () {wmctrl -i -a "$(sed -n $1'p' $bd |awk {'print $2'})"}
+foco () {wmctrl -i -a "$(sed -n $1'p' <<< $(awk {'print $2'} $bd))"}
+close.bkp () {wmctrl -i -c "$(sed -n $1'p' $bd |awk {'print $2'})"}
+close () {wmctrl -i -c "$(sed -n $1'p' <<< $(awk {'print $2'} $bd))"}
 fullscreen () {wmctrl -i -r "$(sed -n $1'p' $bd |awk {'print $2'})" -b toggle,fullscreen}
 monitores () {xrandr|grep -E "'[A-Z]'|connected "|grep -v "disconnect"|awk {'print $1'}|sed -n $1'p'}
 
 #BSPC
 labelmin () {printf '%4s' "$(printf '%-1s' "%{T1}%{T-}") $(sed -n $1'p' $bd|awk {'print "%{T2}"$3"%{T-}"'})"}
 label () {printf '%18s' "$(sed -n $1'p' $bd|awk {'print "%{T2}"$4"%{T-}"'}) $(printf '%17s' "$(sed -n $1'p' $bd|awk {'print $5,$6,$7,$8'}|tail -c 17)") %{T4}$(sed -n $1'p' $bd|awk {'print "%{T3}"$3"%{T-}"'})"}
+#label () {printf '%18s' "$(awk {'print "%{T2}"$4"%{T-}"'} <<< $(sed -n $1'p' $bd)) $(printf '%17s' "$(tail -c 17 <<< $(awk {'print $5,$6,$7,$8'} <<< $(sed -n $1'p' $bd)))) %{T4}$(awk {'print "%{T3}"$3"%{T-}"'} <<< $(sed -n $1'p' $bd))}"
 tiled () {bspc node $(sed -n $1'p' $bd |awk {'print $2'}) -t tiled}
 floating () {bspc node $(sed -n $1'p' $bd |awk {'print $2'}) -t floating}
 sticky () {bspc node $(sed -n $1'p' $bd |awk {'print $2'}) --flag sticky=on}
@@ -21,7 +24,8 @@ i3floating () {i3-msg '[id='$(sed -n $1'p' $bd |awk {'print $2'})']' floating to
 #funções de serviços pro programa
 start () {
 while true; do
-  zsh $path_proj/taskbar.program.zsh
+  #zsh $path_proj/taskbar.program.zsh
+  $path_proj/taskbar.program.zsh
   sleep 0.5
 done
 }
@@ -37,4 +41,6 @@ pid_task=($(ps aux |grep -E '[p]rogram.zsh$|[t]askbar.zsh start$'|awk {'print $2
 kill $pid_task[@]
 }
 
-
+id () {
+cat -n /tmp/taskbar
+}
--- taskbar.program.zsh
+++ taskbar.program.zsh
@@ -14,30 +14,42 @@ test "$scopeold" != "$scopenew" && declare -x scopeold=$scopenew && assignments 
 
 assignments () {
 #separa os ids das janelas em um array.
-declare -x ids=($(echo $scopenew|awk {'print $1'}))
+
+#declare -x ids=($(echo $scopenew|awk {'print $1'}))
+declare -x ids=($(awk {'print $1'} <<< "${(g::)scopenew}"))
 
 #loop para atribuir icones, títulos qual o programa e o workspace com base na quantidade de janelas abertas ${#ids}.
 for (( i=1;  i<=${#ids}; i++ ))
 {
 	#organiza as janelas em lista crescente de workspaces ativos.	
-	indice=$(($(wmctrl -l |grep "${ids[$i]}" |awk {'print $2'}) + 1))
+
+	#indice=$(($(wmctrl -l |grep "${ids[$i]}" |awk {'print $2'}) + 1))
+	indice=$(($(awk {'print $2'} <<< $(grep "${ids[$i]}" <<< "${(g::)scopenew}")) + 1))
+
 	#atribui o ícone do workspace, definido em taskbar.icons.zsh.
 	eval workspace='$'w"$indice" 
+
 	#atribui o título da janela.
-	title=`wmctrl -l |grep "${ids[$i]}" |awk {'print $4,$5,$6'}` 
+	#title=`wmctrl -l |grep "${ids[$i]}" |awk {'print $4,$5,$6'}`
+	title=$(awk {'print $4,$5,$6'} <<< $(grep "${ids[$i]}" <<< "${(g::)scopenew}"))
+
 	#atribui o nome do programa.
-	program=`xwinfo -i ${ids[$i]}|sed 's/\-//g'` 
+	#program=`xwinfo -i ${ids[$i]}|sed 's/\-//g'` 
+	program=$(sed 's/\-//g' <<< "$(xwinfo -i "${ids[$i]}")")
+
 	#atribui o ícone ao programa, definido em taskbar.icons.zsh.
 	eval icon='$'$program 
+
 	#se o programa não estiver definido taskbar.icons.zsh, usar o icon $default.
 	[[ -z $icon ]] && icon=$default 
-	#enquanto o loop rodar para atribuição de janela por janela, salvar o resultado na variável $labels, uma linha por janela aberta.
-	labels="${labels}\n$indice ${ids[$i]} $workspace $icon $program $title" 
+
+	#enquanto o loop rodar para atribuição de janela por janela, salvar o resultado na variável $labels, uma linha por janela ativa.
+	labels+="$indice ${ids[$i]} $workspace $icon $program $title\n"
 }
-#retira linhas em branco de $labels e escreve o resultado em /tmp/taskbar ${bd}.
-grep '[[:digit:]]' <<< "$(echo $labels |sort -n -k1)" > $bd 
+#escreve o conteúdo de $labels ordenado por workspace em /tmp/taskbar ${bd}.
+grep -v '^$' <<< $(sort -nk1 <<< "${(g::)labels}") > $bd 
 #define $labels como nula " " para a proxima rodada de atribuições.
-labels=" "
+unset labels
 #chama a função modules para informar a polybar sobre quais módulos desenhar.
 modules
 }
@@ -49,17 +61,17 @@ modules () {
 for (( i=1; i <= ${#ids}; i++ )) 
 { 
 	[[ ${#ids} -gt 10 ]]&&
-		polybar-msg hook x$i 3 && 
-		sleep 0.05 ||\
-		polybar-msg hook x$i 2 && 
-		sleep 0.05
+		{polybar-msg hook x$i 3
+		sleep 0.05} ||\
+		{polybar-msg hook x$i 2
+		sleep 0.05}
 }
 
 #loop para informar a polybar via ipc-msg (polybar-msg) com base no número de janelas ativas ${#ids};
 #quais o módulos devem ser zerados, apagados ou no caso com label echo "";
 for (( i=$(( ${#ids} + 1 )); i <= $contadorold; i++ ))
 { 
-	polybar-msg hook x$i 1 &&
+	polybar-msg hook x$i 1
 	sleep 0.05 
 }  
 
@@ -77,8 +89,8 @@ start
 
 declare -x scopeold=""
 #valor inicial de $contadorold para assegurar que todos os módulo sejam att no primeiro ciclo.
-declare -x contadorold=20
-#caso o arquivo exista em /tmp/taskbar ele vai criá-lo novamente zerado.
->| $bd
+declare -x contadorold=0
+#caso o arquivo exista em /tmp/taskbar ele vai excluí-lo, em caso de restart.
+[[ -e $bd ]] && rm -f $bd
 #inicia o programa.
 start
